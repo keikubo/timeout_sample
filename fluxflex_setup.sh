b199d50bc8d33f45de7cf54ee8affa0cb3292639
Kei Kubo <keikubo@gmail.com>
2011-07-08 10:08:18 -0500

    added time.sleep()

--- fluxflex_setup.sh
+++ fluxflex_setup.sh
@@ -3,3 +3,4 @@ export GEM_PATH=$GEM_HOME
 export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.8.7-p334/bin:$HOME/.gem/ruby/1.8/bin
 rake db:migrate:reset RAILS_ENV="production"
 echo "ja" | rake redmine:load_default_data RAILS_ENV="production" 
+python test.py
